b19c693db2c605971a765880989e5f0db5a162df
Yuan Chenxuan <yuan.chenxuan@u.nus.edu>
2021-10-18 18:58:19 +0800

    First Commit

---
+++ HotelReservationSystem/HotelReservationSystem-ejb/src/java/ejb/session/singleton/DataInitSessionBean.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.singleton;
+
+import ejb.session.stateless.EmployeeEntitySessionBeanLocal;
+import entity.Employees;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.LocalBean;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author chenx
+ */
+@Singleton
+@LocalBean
+@Startup
+public class DataInitSessionBean {
+
+    @EJB
+    private EmployeeEntitySessionBeanLocal employeeEntitySessionBeanLocal;
+
+    @PersistenceContext(unitName = "HotelReservationSystem-ejbPU")
+    private EntityManager em;
+
+    
+
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @PostConstruct
+    public void postConstruct() {
+        employeeEntitySessionBeanLocal.createNewEmployee(new Employees("employee1", "Sales Manager", "123"));
+        employeeEntitySessionBeanLocal.createNewEmployee(new Employees("employee2", "Sales Manager", "123"));
+        employeeEntitySessionBeanLocal.createNewEmployee(new Employees("employee3", "Sales Manager", "123"));
+    }
+    
+}
---
+++ HotelReservationSystem/HotelReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeEntitySessionBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.Employees;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author chenx
+ */
+@Stateless
+public class EmployeeEntitySessionBean implements EmployeeEntitySessionBeanRemote, EmployeeEntitySessionBeanLocal {
+
+    @PersistenceContext(unitName = "HotelReservationSystem-ejbPU")
+    private EntityManager em;
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+    @Override
+    public List<Employees> retrieveAllEmployees() {
+        Query query = em.createQuery("Select e FROM Employees e");
+        
+        return query.getResultList();
+    }
+    
+    @Override
+    public String createNewEmployee(Employees newEmployee) {
+        em.persist(newEmployee);
+        
+        return newEmployee.getUsername();
+    }
+}
